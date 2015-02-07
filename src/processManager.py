289e744952f1e6f240411f257facbc8fd1e3f4ac
ianwcarlson <ianwcarlson@gmail.com>
2015-02-07 12:13:42 -0700

    made process manager into class

--- src/processManager.py
+++ src/processManager.py
@@ -10,33 +10,56 @@ import time
 
 scriptDir=os.path.dirname(os.path.realpath(__file__))
 sys.path.append(scriptDir)
-import toZeroMQ
-import appConfigConstants
 
 import pdb
 
-def run():
+class ProcessManager():
+    def __init__(self):
+        """
+        Constructor.  Initializes class variables.
+        """
+        self.processHandleList = []
 
-    processHandleList = []
+    def addProcess(self, inProcessDict):
+        """
+        Add process executable to managed list
 
-    # start processes
-    for idx in range(len(appConfigConstants.processList)):
-        path = os.path.join(scriptDir,appConfigConstants.processList[idx]['processName'])
-        print ('opening process: ' + str(path))
-        processHandleList.append(subprocess.Popen(['python3', path], stdout=True))
+        :param inProcessDict: dictionary containing 'processName' and 'endPoint' \
+        keys that correspond to the python file to run and the IP address of each
+        node in the system respectively
+        :type inProcessDict: dictionary
+        :raises ValueError
+        """
+        if ('processName' in inProcessDict and 'endPoint' in inProcessDict):
+            self.processHandleList.append(inProcessDict)
+        else:
+            raise ValueError("'processName' or 'endPoint' keys don't exist in input dictionary")
+        
 
-    # monitor processes to see if they're still aliveps
-    while(True):
-        if (len(processHandleList) == 0):
-            break
+    def run(self):
+        """
+        Run all processes in process list.  This should evolve to be more responsive \
+        to subprocess handling.  For now, this class assumes python processes but could \
+        be extended to any executable if the permissions and sourced interpretor was \
+        configured properly
 
-        for idx in range(len(processHandleList)):
-            if (processHandleList[idx].poll() != None):
-                print('process done')
-                # only pop one at a time because doing so screws up indexing
-                processHandleList.pop(idx)
+        """
+        # start processes
+        for idx in range(len(self.processHandleList)):
+            path = os.path.join(scriptDir,self.processHandleList[idx]['processName'])
+            print ('opening process: ' + str(path))
+            processHandleList.append(subprocess.Popen(['python3', path], stdout=True))
+
+        # monitor processes to see if they're still aliveps
+        while(True):
+            if (len(self.processHandleList) == 0):
                 break
 
-        time.sleep(0.1)
+            for idx in range(len(self.processHandleList)):
+                if (self.processHandleList[idx].poll() != None):
+                    print('process done')
+                    # only pop one at a time because doing so screws up indexing
+                    self.processHandleList.pop(idx)
+                    break
 
-run()
+            time.sleep(0.1)
