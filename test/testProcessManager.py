5ece12889af545591022530a68a0ffdef714df1d
ianwcarlson <ianwcarlson@gmail.com>
2015-02-07 13:04:49 -0700

    got first unit test working for process manager

--- test/testProcessManager.py
+++ test/testProcessManager.py
@@ -16,7 +16,7 @@ class TestProcessManager(unittest.TestCase):
     def setUp(self):
         self.processManager = processManager.ProcessManager()
 
-    def test_add_process(self):
+    def test_add_good_process(self):
         newProcess = {
         	'processName': os.path.join(scriptDir,'testFixtures','process1.py'),
         	'endPoint': 'tcp://127.0.0.1:5556'
@@ -27,6 +27,13 @@ class TestProcessManager(unittest.TestCase):
         for key in newProcess:
         	self.assertEqual(newProcess[key], getNewProcess[0][key], 
         		str(newProcess[key]) + " did not match " + str(getNewProcess[0][key]))
+
+    def test_add_bad_process(self):
+    	newProcess = {
+        	'processme': os.path.join(scriptDir,'testFixtures','process1.py'),
+        	'endint': 'tcp://127.0.0.1:5556'
+        }
+        self.assertRaises(ValueError, self.processManager.addProcess(newProcess))
 
 
 if __name__ == '__main__':
