a073d41d8a951da01dd2115eca782bdce833a8d3
ianwcarlson <ianwcarlson@gmail.com>
2015-03-02 19:41:09 -0700

    cleaning up the server print statements

--- examples/geofencing/webServer/server.js
+++ examples/geofencing/webServer/server.js
@@ -3,30 +3,23 @@ module.exports = function(){
 	var app = express();
 	var http = require('http').Server(app);
 	var io = require('socket.io')(http);
-		// Publisher.  Need to write javascript bindings for processNode.py
+	// Publisher.  Need to write javascript bindings for processNode.py
 	var zmq = require('zmq')
 	  , sockPub = zmq.socket('pub');
 	var pubEndPointAddress = 'tcp://127.0.0.1:5562';
 	sockPub.bindSync(pubEndPointAddress);
 
 	app.get('/', function(req, res){
-	  console.log('sending index file');
 	  res.sendFile(__dirname + '/index.html');
 	});
 
 	app.use(express.static(__dirname + '/dist'));
 
 	io.on('connection', function(socket){
-		console.log('connected to client');
-		socket.on('chat message', function(msg){
-			io.emit('chat message', msg);
-		});
-		socket.on('newPolygonPoints', function(newPolygonPoints){
-			
+		socket.on('newPolygonPoints', function(newPolygonPoints){			
 			var newMsg = {};
 			newMsg['endPointAddress'] = pubEndPointAddress;
 			newMsg['contents'] = newPolygonPoints;
-			console.log('sending new poly points: ', newMsg);
 			sockPub.send(['newPolygonPoints', JSON.stringify(newMsg)]);
 		});
 	});
@@ -39,17 +32,15 @@ module.exports = function(){
 	sockSubPolygon = zmq.socket('sub');
 	sockSubPolygon.connect('tcp://127.0.0.1:5561');
 	sockSubPolygon.subscribe('pointInPolygon');
-	console.log('Subscriber connected to port 3000');
+	console.log('Subscriber connected to port 5561');
 	pointInPolygon = false;
 
 	sockSubPolygon.on('message', function(topic, message) {
 		topic = new Buffer(topic).toString('utf-8');
 		message = new Buffer(message).toString('utf-8');
 		message = JSON.parse(message);
-		console.log('received a message related to:', topic, 'containing message:', message);
 		if (topic === 'pointInPolygon'){		
 			if (message['contents'] != pointInPolygon){
-				console.log('New polygon status!!!!!!!!!!!!!!!!!!!!!');
 				io.emit('pointInPolygon', message['contents']);
 				pointInPolygon = message['contents'];	
 			}
@@ -59,13 +50,12 @@ module.exports = function(){
 	sockSubGpsData = zmq.socket('sub');
 	sockSubGpsData.connect('tcp://127.0.0.1:5550');
 	sockSubGpsData.subscribe('gpsData');
-	console.log('Subscriber connected to port 3000');
+	console.log('Subscriber connected to port 5550');
 
 	sockSubGpsData.on('message', function(topic, message) {
 		topic = new Buffer(topic).toString('utf-8');
 		message = new Buffer(message).toString('utf-8');
 		message = JSON.parse(message);
-		//console.log('received a message related to:', topic, 'containing message:', message);
 		if (topic == 'gpsData'){
 			io.emit('newGpsPoint', message['contents']);
 		}
