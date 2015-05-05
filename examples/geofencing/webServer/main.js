dda132b28ae3b712aaed314877167d59bd6309aa
ianwcarlson <ianwcarlson@gmail.com>
2015-05-05 15:35:45 -0600

    deleted main.js

--- examples/geofencing/webServer/main.js
+++
@@ -1,9 +0,0 @@
-// Spin up server
-require('./server.js')();
-
-//var http = require('http');
-//http.createServer(function (req, res) {
-//  res.writeHead(200, {'Content-Type': 'text/plain'});
-//  res.end('Hello World\n');
-//}).listen(3698, '127.0.0.1');
-//console.log('Server running at http://127.0.0.1:3698/');
